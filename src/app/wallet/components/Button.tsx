085441512c3ac04f3a4cdcceb9cf76f26e97111e
Hendrik Grondijs <hlgrondijs@gmail.com>
2023-12-28 11:07:43 +0100

    disable button, tnc

--- src/app/wallet/components/Button.tsx
+++ src/app/wallet/components/Button.tsx
@@ -2,14 +2,24 @@ import { ReactNode } from "react";
 
 type ButtonProps = {
   onClick: () => void;
+  disabled?: boolean;
   children: ReactNode;
 };
 
-export default function Button({ onClick, children }: ButtonProps) {
+export default function Button({
+  onClick,
+  disabled = false,
+  children,
+}: ButtonProps) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
-      className="rounded-lg border border-white bg-white px-5 py-4 transition-colors hover:border-gray-300 hover:bg-transparent hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      className={`rounded-lg border border-white px-5 py-4  ${
+        disabled
+          ? "bg-transparent text-gray-500"
+          : "  bg-white text-black transition-colors hover:border-gray-300 hover:bg-transparent hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      }`}
     >
       {children}
     </button>
--- src/app/wallet/containers/SelectWallet.tsx
+++ src/app/wallet/containers/SelectWallet.tsx
@@ -10,6 +10,7 @@ export default function SelectWallet() {
   const { connectWallet, connectionError } = useAppContext();
   const [selectedWalletType, setSelectedWalletType] =
     useState<WalletType | null>(null);
+  const [tncAccepted, setTncAccepted] = useState<boolean>(false);
 
   const selectWallet = (wType: WalletType) => {
     if (selectedWalletType === wType) {
@@ -22,7 +23,11 @@ export default function SelectWallet() {
   return (
     <div className="flex flex-col justify-start items-center w-full flex-1 mt-8">
       <div className="flex flex-col items-center">
-        <p>Choose a wallet</p>
+        {selectedWalletType ? (
+          <b>{`${selectedWalletType}`}</b>
+        ) : (
+          <p>Choose a wallet</p>
+        )}
         <div className="flex justify-center gap-8 mt-4">
           <SelectWalletButton
             walletType={WalletType.ALBEDO}
@@ -35,26 +40,36 @@ export default function SelectWallet() {
             onClick={() => selectWallet(WalletType.XBULL)}
           />
         </div>
+        <div className="mt-4 gap-2">
+          <input
+            type="checkbox"
+            checked={tncAccepted}
+            onChange={() => setTncAccepted(!tncAccepted)}
+            id="checkbox_policy"
+          />
+          <label className="p-4 mb-4 cursor-pointer" htmlFor="checkbox_policy">
+            I agree with privacy policy.
+          </label>
+        </div>
       </div>
-      {selectedWalletType !== null ? (
-        <div className="flex flex-col items-center gap-4 mt-16 w-80">
-          <b>{`${selectedWalletType} selected.`}</b>
-          <Button
-            onClick={() => {
-              connectWallet(selectedWalletType);
-              setSelectedWalletType(null);
-            }}
-          >
-            Connect
-          </Button>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center text-center gap-4 mt-16 w-80">
-          {connectionError && (
-            <span className="text-red-500">{connectionError}</span>
-          )}
-        </div>
-      )}
+
+      <div className="flex flex-col items-center gap-4 mt-16 w-80">
+        <Button
+          disabled={selectedWalletType === null || !tncAccepted}
+          onClick={() => {
+            connectWallet(selectedWalletType!);
+            setSelectedWalletType(null);
+          }}
+        >
+          Connect
+        </Button>
+      </div>
+
+      <div className="flex flex-col items-center text-center gap-4 mt-16 w-80">
+        {connectionError && (
+          <span className="text-red-500">{connectionError}</span>
+        )}
+      </div>
     </div>
   );
 }
