4377dc371eb088550784963c7a769b8038e24852
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-01-25 14:08:06 +0100

    add privacy policy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Footer() {
@@ -9,9 +10,11 @@ export default function Footer() {
       <div className="py-4 grow flex flex-wrap items-center justify-center gap-2 gap-x-8 md:gap-x-12">
         <span className="text-gray-500">© 2023 Stellarcarbon</span>
 
-        <span>Terms of services</span>
-        <span>Privacy Policy</span>
-        <span className="text-sm">support@stellarcarbon.io</span>
+        {/* <span>Terms of services</span> */}
+        <Link href="/privacy-policy">Privacy Policy</Link>
+        <a className="underline text-sm" href="mailto:support@stellarcarbon.io">
+          support@stellarcarbon.io
+        </a>
         <img className="w-6 h-6" src="/x-logo-white.png" />
       </div>
     </div>
