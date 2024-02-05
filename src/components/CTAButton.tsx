3ef1c052cbaba3117cb4a7549341d04f1b2f2507
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-02-05 09:51:44 +0100

    add CTAButton onclick

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,13 +1,17 @@
 import { HTMLProps, ReactNode } from "react";
 import BuyStellarCarbonIcon from "./icons/BuyStellarCarbon";
+import { useRouter } from "next/navigation";
 
 interface CTAButtonProps extends HTMLProps<HTMLButtonElement> {}
 
-export default function CTAButton({ onClick, className = "" }: CTAButtonProps) {
+export default function CTAButton({ className = "" }: CTAButtonProps) {
+  const router = useRouter();
   return (
     <button
       type="button"
-      onClick={onClick}
+      onClick={() => {
+        router.push("/wallet");
+      }}
       className={`p-1 w-[250px] flex justify-center bg-accent text-black uppercase rounded-2xl shadow-[0px_11px_40px_1px_rgba(255,255,255,0.3)] hover:bg-primary hover:text-white ${className}`}
     >
       <BuyStellarCarbonIcon />
