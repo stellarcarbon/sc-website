01853fce36888fc3e3be12497c7db51d8cf219c4
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-04-24 09:57:27 +0200

    add double chevron down and update rainforestbg section

---
+++ src/components/icons/DoubleChevronDownIcon.tsx
@@ -0,0 +1,23 @@
+export default function DoubleChevronDownIcon() {
+  return (
+    <svg
+      width="96px"
+      height="96px"
+      viewBox="0 0 21 21"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        transform="translate(6 6)"
+      >
+        <path d="m8.5.5-4 4-4-4" />
+
+        <path d="m8.5 4.5-4 4-4-4" />
+      </g>
+    </svg>
+  );
+}
--- src/containers/home/RainforestSection.tsx
+++ src/containers/home/RainforestSection.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import CTAButton from "@/components/CTAButton";
-import { useEffect } from "react";
+import DoubleChevronDownIcon from "@/components/icons/DoubleChevronDownIcon";
+import { useEffect, useRef, useState } from "react";
 
 export default function RainforestIntro() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const chevDown = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     function adjustBackgroundSize() {
       const viewportHeight = window.innerHeight;
@@ -29,11 +35,39 @@ export default function RainforestIntro() {
     window.addEventListener("orientationchange", adjustBackgroundSize);
   }, []);
 
+  const trackScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", trackScroll, { passive: true });
+    setScrollPosition(0);
+
+    const chev = chevDown.current;
+    const handleAnimationEnd = () => {
+      if (chev) {
+        chev.style.visibility = "hidden";
+      }
+    };
+
+    chev?.addEventListener("animationend", handleAnimationEnd);
+
+    return () => {
+      window.removeEventListener("scroll", trackScroll);
+      chev?.removeEventListener("animationend", handleAnimationEnd);
+    };
+  }, []);
+
+  const onClickChevDown = () => {
+    window.scrollTo({ left: 0, top: window.innerHeight, behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="bg-rainforest bg-cover bg-top bg-fixed bg-no-repeat w-full top-0 left-0 z-0 block absolute" />
-      <div className="hero-text flex flex-col items-center justify-center gap-[80px] z-10 text-center w-[100%] max-h-[50%]">
-        <div className="flex flex-col leading-8 text-md px-5 py-8 w-full bg-primary/[.54]">
+      <div className="min-h-[calc(100vh-80px)] hero-text flex flex-col items-center justify-center gap-[80px] z-10 text-center w-[100%]">
+        <div className="flex flex-col h-[50%] leading-8 text-md px-5 py-8 w-full bg-primary/[.54]">
           <h1 className="px-4 text-4xl lg:text-5xl font-noto mb-4">
             Sinking CARBON with Stellar
           </h1>
@@ -51,6 +85,15 @@ export default function RainforestIntro() {
             emissions.
           </span>
           <CTAButton className="self-center mt-4 md:mt-8" />
+        </div>
+        <div
+          ref={chevDown}
+          className={`absolute cursor-pointer bottom-0 animate-bounce ${
+            scrollPosition > 0 ? "animate-fade" : ""
+          }`}
+          onClick={onClickChevDown}
+        >
+          <DoubleChevronDownIcon />
         </div>
       </div>
     </>
