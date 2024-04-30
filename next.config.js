0b24a1ae805ac8e501ac302371c9b66e9606a22a
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-04-30 13:48:47 +0200

    fix e2e test script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+let output = "export";
+if (process.env.NODE_ENV === "test") {
+  // Static export doesn't work with current e2e script.
+  console.log('>>>> NODE_ENV set to "test". Using standalone output.');
+  output = "standalone";
+}
+
 const nextConfig = {
-  // Settings from digitalocean nextjs sample https://github.com/digitalocean/sample-nextjs/
-  output: "export",
+  output,
   distDir: "out",
   images: {
     unoptimized: true,
