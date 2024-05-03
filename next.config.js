2d35f8977daeabbe1821939c478dc86529d64c18
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-05-03 16:22:35 +0200

    trailingSlash is true when exporting to static

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 let output = "export";
+let trailingSlash = true;
 if (process.env.NODE_ENV === "test") {
   // Static export doesn't work with current e2e script.
   console.log('>>>> NODE_ENV set to "test". Using standalone output.');
   output = "standalone";
+  trailingSlash = false;
 }
 
 const nextConfig = {
   output,
+  trailingSlash,
   distDir: "out",
   images: {
     unoptimized: true,
