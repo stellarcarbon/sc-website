f9333011f91e75381959c9726892f3572e43ae79
Hendrik Grondijs <hlgrondijs@gmail.com>
2024-06-04 10:24:49 +0200

    cleanup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import Drawer from "@/components/Drawer";
 import Footer from "@/components/Footer";
 import TopBar from "@/components/TopBar";
 import { useAppContext } from "@/context/appContext";
-import { useEffect, useState } from "react";
 
 export default function App({ children }: { children: React.ReactNode }) {
   const { isDrawerOpen } = useAppContext();
