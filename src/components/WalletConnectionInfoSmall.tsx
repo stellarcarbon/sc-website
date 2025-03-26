244b7cd1bcd405ea940e7794cffd65d8744fa7f1
Hendrik Grondijs <hlgrondijs@gmail.com>
2025-03-26 12:13:48 +0100

    slight edit

--- src/components/WalletConnectionInfoSmall.tsx
+++ src/components/WalletConnectionInfoSmall.tsx
@@ -5,14 +5,13 @@ export default function WalletConnectionInfoSmall() {
   const { walletConnection } = useAppContext();
 
   return (
-    <div className="flex bg-darker text-white border border-accentSecondary rounded cursor-pointer md:hover:bg-secondary md:hover:text-white">
-      <div className="flex flex-col items-center border-rl border-accentSecondary p-1 px-2">
-        <img
-          className="h-5 w-5 md:h-6 md:h-6"
-          src={walletConnection?.walletType.icon}
-        />
-      </div>
-      <div className="flex items-center pr-2 text-xs">
+    <div className="flex items-center gap-1 bg-darker text-white border border-accentSecondary rounded cursor-pointer md:hover:bg-secondary md:hover:text-white h-8 md:h-10 px-2">
+      <img
+        className="h-5 w-5 md:h-6 md:w-6"
+        src={walletConnection?.walletType.icon}
+      />
+
+      <div className="flex items-center text-xs md:text-sm">
         <TruncatedHash hash={walletConnection?.stellarPubKey} uppercase />
       </div>
     </div>
