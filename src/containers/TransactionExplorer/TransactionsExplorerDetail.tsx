d0b9f497c6d7ec7eb39daa8863f5e311dcc0ee33
Hendrik Grondijs <hlgrondijs@gmail.com>
2025-04-07 10:50:47 +0200

    fix stellar expert url template

--- src/containers/TransactionExplorer/TransactionsExplorerDetail.tsx
+++ src/containers/TransactionExplorer/TransactionsExplorerDetail.tsx
@@ -150,8 +150,8 @@ export default function TransactionsExplorerDetail() {
         <Link
           href={
             appConfig.network === WalletNetwork.PUBLIC
-              ? `https://stellar.expert/public/tx/${transaction.id}`
-              : `https://stellar.expert/testnet/tx/${transaction.id}`
+              ? `https://stellar.expert/explorer/public/tx/${transaction.id}`
+              : `https://stellar.expert/explorer/testnet/tx/${transaction.id}`
           }
           target="_blank"
           className="text-accentSecondary underline my-4 col-span-5 text-center"
