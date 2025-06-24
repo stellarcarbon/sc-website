1e2b376b5488038c20976173642325ce51eb572a
Hendrik Grondijs <hlgrondijs@gmail.com>
2025-06-24 16:10:30 +0200

    analytics consent

--- src/components/AnalyticsConsent.tsx
+++ src/components/AnalyticsConsent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import posthog from "posthog-js";
 import { useState, useEffect } from "react";
 
 export default function AnalyticsConsent() {
@@ -22,8 +23,8 @@ export default function AnalyticsConsent() {
     }
     setVisible(false);
     // Optionally initialize analytics here if consent === true
-    if (consent) {
-      // initializeAnalytics();
+    if (!consent) {
+      posthog.opt_out_capturing();
     }
   };
 
@@ -32,10 +33,16 @@ export default function AnalyticsConsent() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-2xl shadow-lg max-w-md text-center mx-4">
+        <h2 className="text-xl font-semibold mb-4">Test Version dApp</h2>
+        <p className="mb-3 text-gray-700">
+          {`This is a testnet environment that doesn’t handle real money or real
+          carbon credits. Please set your wallet to the Stellar Testnet before
+          connecting.`}
+        </p>
         <h2 className="text-xl font-semibold mb-4">Analytics Consent</h2>
-        <p className="mb-6 text-gray-700">
-          We’d like to collect anonymous analytics data on this deployment to
-          help improve the app. Your choice will be saved in your browser.
+        <p>
+          {`We’d like to collect anonymous analytics data to help us improve the app. Your
+          choice will be stored locally in your browser.`}
         </p>
         <div className="flex justify-center space-x-4">
           <button
