14193218d19de83d8cd95c79127944a5f4857a19
Alex Olieman <alex@olieman.net>
2026-01-25 16:09:10 +0100

    footer: add Stellar trademark disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="w-full h-24 flex flex-col justify-center items-center shadow-md-inverted bg-darker text-accent border-t border-t-secondary">
+    <div className="w-full h-50 flex flex-col justify-center items-center shadow-md-inverted bg-darker text-accent border-t border-t-secondary">
       <div className="py-4 grow flex flex-wrap items-center justify-center gap-2 gap-x-8 md:gap-x-12">
         <span className="text-gray-500">© 2025 Stellarcarbon</span>
 
@@ -24,6 +24,11 @@ export default function Footer() {
           />
         </Link>
       </div>
+      <p className="px-4 italic text-gray-400 text-center">
+          {`“Stellar”`} is a trademark of the Stellar Development Foundation. All rights reserved.
+          Stellarcarbon is an independent project, not affiliated with, sponsored or endorsed by 
+          the Stellar Development Foundation.
+      </p>
     </div>
   );
 }
